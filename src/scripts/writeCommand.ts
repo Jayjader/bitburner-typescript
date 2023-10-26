fa9808cd7fd3e9f61a3a5533e48fca7b8ea3eafb
jayjader <jayjader@desktop>
2023-10-26 02:02:57 +0200

    allow sending multiple commands at once to the excutor via writeCommand script

--- src/scripts/writeCommand.ts
+++ src/scripts/writeCommand.ts
@@ -1,9 +1,9 @@
-import { NS } from '@ns'
-import { ports } from 'scripts/constants.js'
+import { NS } from "@ns";
+import { ports } from "scripts/constants.js";
 
 export async function main(ns: NS) {
-  const popped = ns.writePort(ports.commandBus, String(ns.args[0]))
+  const popped = ns.writePort(ports.commandBus, JSON.stringify(ns.args));
   if (popped !== null) {
-    ns.tprintf(`queue full; this was popped from it when writing: ${popped}`)
+    ns.tprintf(`queue full; this was popped from it when writing: ${popped}`);
   }
 }
