b0f2479d936dcabe337483614e357c7715ba27a2
jayjader <jayjader@desktop>
2023-10-27 00:36:30 +0200

    special script to command cracks of all hacking faction related servers

---
+++ src/scripts/crack-factions.ts
@@ -0,0 +1,11 @@
+import { NS } from "@ns";
+
+const servers = ["CSEC", "avmnite-02h", "I.I.I.I", "run4theh111z", "The-Cave"];
+
+export async function main(ns: NS) {
+  ns.run(
+    "scripts/writeCommand.js",
+    1,
+    servers.map((server) => `crack:${server}`).join(" "),
+  );
+}
