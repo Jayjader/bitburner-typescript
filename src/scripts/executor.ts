6c8a40a85744f206bc3c2f2d834613dc0b4b0b36
jayjader <jayjader@desktop>
2023-10-27 05:28:01 +0200

    fix imports

--- src/scripts/executor.ts
+++ src/scripts/executor.ts
@@ -1,5 +1,5 @@
 import { NS } from "@ns";
-import { ports } from "scripts/constants.js";
+import { ports } from "/scripts/constants.js";
 import {
   type RunningWorker,
   scanForRunningWorkers,
--- src/scripts/writeCommand.ts
+++ src/scripts/writeCommand.ts
@@ -1,5 +1,5 @@
 import { NS } from "@ns";
-import { ports } from "scripts/constants.js";
+import { ports } from "/scripts/constants.js";
 
 export async function main(ns: NS) {
   const popped = ns.writePort(ports.commandBus, JSON.stringify(ns.args));
