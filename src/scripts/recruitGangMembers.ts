350e33f39bbed7d8436eaaeb64f4a07c0e324240
jayjader <jayjader@desktop>
2023-11-09 20:31:54 +0100

    bugfix: wait to be in gang before recruiting

--- src/scripts/recruitGangMembers.ts
+++ src/scripts/recruitGangMembers.ts
@@ -1,6 +1,9 @@
 import { NS } from "@ns";
 
 export async function main(ns: NS) {
+  while (!ns.gang.inGang()) {
+    await ns.sleep(1000);
+  }
   while (true) {
     await ns.sleep(1_000);
     if (!ns.gang.canRecruitMember()) {
