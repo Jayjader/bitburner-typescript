60f6e0b56ebdd8b9c9b6b8636d765575886ceac0
jayjader <jayjader@desktop>
2023-11-19 13:49:46 +0100

    allow passing a specific port as arg to port flusher script

--- src/scripts/flushPort.ts
+++ src/scripts/flushPort.ts
@@ -2,8 +2,9 @@ import type { NS } from "@ns";
 import { ports } from "/scripts/constants";
 
 export async function main(ns: NS) {
-  const handle = ns.getPortHandle(ports.commandBus);
-  while (handle.peek() !== "NULL PORT DATA") {
+  const port = (ns.args.at(0) as number | undefined) ?? ports.commandBus;
+  const handle = ns.getPortHandle(port);
+  while (!handle.empty()) {
     ns.tprintf(`${handle.read()}`);
   }
 }
